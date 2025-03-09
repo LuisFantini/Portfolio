5323f44f015a03973eb382227dcbd0379f44a1db
Rafael Lopes <rafaeldeoliveiracl@gmail.com>
2025-03-09 02:30:53 -0300

    Add assets + nav-pc e nav-mobile

---
+++ assets/script.js
@@ -0,0 +1,6 @@
+function ativaMenu() {
+    const menu = document.querySelector('.links-menu');
+    const icon = document.querySelector('.icone-mobile');
+    menu.classList.toggle('ativo');
+    icon.classList.toggle('ativo');
+}
